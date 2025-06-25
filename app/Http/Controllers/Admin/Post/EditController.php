83b904f9aaf3c23cb473c647132cdb9b185228fd
IvanTsumbek <maincumbek@gmail.com>
2025-06-25 23:48:16 +0300

    19 Lesson HARD!!!

--- app/Http/Controllers/Admin/Post/EditController.php
+++ app/Http/Controllers/Admin/Post/EditController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers\Admin\Post;
 
+use App\Models\Tag;
 use App\Models\Post;
+use App\Models\Category;
 use App\Http\Controllers\Controller;
 
 class EditController extends Controller
 {
     public function __invoke(Post $post)
     {
-        return view('admin.post.edit', compact('post'));
+                $categories = Category::all();
+        $tags = Tag::all();
+        return view('admin.post.edit', compact('post', 'categories', 'tags'));
     }
 }
--- app/Http/Controllers/Admin/Post/UpdateController.php
+++ app/Http/Controllers/Admin/Post/UpdateController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin\Post;
 
 use App\Models\Post;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
 use App\Http\Requests\Admin\Post\UpdateRequest;
 
 class UpdateController extends Controller
@@ -11,7 +12,13 @@ class UpdateController extends Controller
     public function __invoke(UpdateRequest $request, Post $post)
     {
         $data = $request->validated();
+        $tagIds = $data['tag_ids'];
+        unset($data['tag_ids']);
+
+        $data['preview_image'] = Storage::disk('public')->put('/images',  $data['preview_image']);    //справа кладем файл, слева переменная с путем
+        $data['main_image'] = Storage::disk('public')->put('/images',  $data['main_image']);
         $post->update($data);
+        $post->tags()->sync($tagIds);
         return view('admin.post.show', compact('post'));
     }
 }
