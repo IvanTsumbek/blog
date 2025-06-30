1261ba0cff8d05484c278bf5d06700629d70cd98
IvanTsumbek <maincumbek@gmail.com>
2025-06-30 21:47:11 +0300

    28 Lesson Отправление письма с подтверждением авторизации

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::group(['namespace' => 'Main'], function () {
     Route::get('/', 'IndexController');
 });
 
-Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' => ['auth','admin']], function () {
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' => ['auth','admin', 'verified']], function () {
     Route::group(['namespace' => 'Main'], function () {
         Route::get('/', 'IndexController');
     });
@@ -64,7 +64,4 @@ Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' => ['aut
 });
 
 
-
-
-
-Auth::routes();
+Auth::routes(['verify' => true]);
